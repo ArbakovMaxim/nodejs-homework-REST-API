9a9dfb7f34fd7a60a368ae0fbca446b9da47e0b8
Arbakov Maxim <fefaodessa@gmail.com>
2022-10-09 17:28:20 +0300

    DZ 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const ctrl = require("../../controllers");
+const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
 const { validateBody } = require("../../middlewares");
-const schemas = require("../../schema");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
@@ -10,10 +10,20 @@ router.get("/", ctrlWrapper(ctrl.getAll));
 
 router.get("/:contactId", ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(schemas), ctrlWrapper(ctrl.add));
+router.post("/", validateBody(schemas.addScheme), ctrlWrapper(ctrl.add));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.deleteById));
 
-router.put("/:contactId", validateBody(schemas), ctrlWrapper(ctrl.updateById));
+router.put(
+  "/:contactId",
+  validateBody(schemas.addScheme),
+  ctrlWrapper(ctrl.updateById)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  validateBody(schemas.updateFavoriteScheme),
+  ctrlWrapper(ctrl.updateFavorit)
+);
 
 module.exports = router;
