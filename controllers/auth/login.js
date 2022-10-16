7cceb63f665c0d2c080888156b0b9e7146593b98
Arbakov Maxim <fefaodessa@gmail.com>
2022-10-16 12:22:16 +0300

    DZ 4 token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../../models/user");
 const { RequestError } = require("../../helpers");
-const { SECRET_KEY } = process.evn;
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
